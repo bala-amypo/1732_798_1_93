1590055b1517364649d41d19d1d885b053d7374c
bala-amypo <bala@amypo.in>
2025-12-18 06:05:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/servlent/SimpleHelloServlet.java
@@ -0,0 +1,20 @@
+package com.example.demo.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SimpleHelloServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws IOException {
+        response.setContentType("text/plain");
+        response.setStatus(200);
+        PrintWriter out = response.getWriter();
+        out.print("Hello form Simple Hello Servlet");
+        out.flush();
+    }
+}
