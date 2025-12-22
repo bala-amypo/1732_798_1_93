d8ace5f7d6477465857461c42982fb17e8430970
bala-amypo <bala@amypo.in>
2025-12-22 09:27:45 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package com.example.demo.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -15,8 +19,9 @@ public class SwaggerConfig {
                 // You need to change the port as per your server
                 .servers(List.of(
                         new Server().url("https://9134.408procr.amypo.ai/")
-                ));
-                .title("Medication Catalog API")
+                ))
+                .info(new Info()
+                        .title("Medication Catalog API")
                         .version("1.0")
                         .description("API for managing medications and ingredients"))
                 .components(new Components()
@@ -25,6 +30,5 @@ public class SwaggerConfig {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
-        }
+    }
 }
-
