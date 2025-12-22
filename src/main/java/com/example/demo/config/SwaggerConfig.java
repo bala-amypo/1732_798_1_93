2c2a28a6230e977c3f0a45600641962478fff13c
bala-amypo <bala@amypo.in>
2025-12-22 09:30:45 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
@@ -15,20 +16,27 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        
         return new OpenAPI()
-                // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9134.408procr.amypo.ai/")
+                        new Server()
+                                .url("https://9134.408procr.amypo.ai/")
+                                .description("Production Server")
                 ))
                 .info(new Info()
                         .title("Medication Catalog API")
                         .version("1.0")
                         .description("API for managing medications and ingredients"))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
                 .components(new Components()
-                        .addSecuritySchemes("bearer-key",
+                        .addSecuritySchemes(securitySchemeName,
                                 new SecurityScheme()
+                                        .name(securitySchemeName)
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT")));
+                                        .bearerFormat("JWT")
+                                        .description("Provide JWT token with 'Bearer ' prefix")));
     }
 }
