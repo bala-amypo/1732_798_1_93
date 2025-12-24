d06cb06c6f25cf4bda244ae65bd235cada8d5889
bala-amypo <bala@amypo.in>
2025-12-24 05:58:32 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -18,3 +18,6 @@ public class SwaggerConfig {
                 ));
         }
 }
+
+curl -v "https://9172.408proc.cr.com/api/auth/register" -H "*/*" -H "Content-Type: application/json" -d '{
+"username":  "abc","email": "abc@gmail.com","password": "abc123" }'
